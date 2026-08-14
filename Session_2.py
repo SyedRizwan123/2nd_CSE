5be4fc61b6614d89d0977729dee715a6a1ceae2f
Syed Rizwan <syedbbictech@gmail.com>
2026-08-14 13:04:50 +0530

    Add files via upload

---
+++ Session_2.py
@@ -0,0 +1,12 @@
+"""name="Syed"
+print(name)"""
+
+"""name=input("Enter a name")
+print("Hello",name)"""
+
+"""num=input("Enter your number")
+print("you entered the number", num)
+print(type(num))"""
+
+num = int(input("Enter a number: "))
+print(num + 5)
